c9f24a500de91f5f6af84869f6e19189fbfe81d8
alcabanillas <acabanillas@gmail.com>
2023-02-19 19:21:29 +0100

    unificar autenticacion

--- authService.js
+++ authService.js
@@ -1,10 +1,23 @@
 //Initialize firebaseApp if it has not been done yet
-const firebaseApp = require("./admin")
+const { initializeApp } = require("firebase/app");
 const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
 
-const auth = getAuth();
+const firebaseConfig = {
+  apiKey: "AIzaSyCIdgP5UepZYNJfegdUxhEaZl_lCNz3m8U",
+  authDomain: "badbank-87112.firebaseapp.com",
+  projectId: "badbank-87112",
+  storageBucket: "badbank-87112.appspot.com",
+  messagingSenderId: "641621008693",
+  appId: "1:641621008693:web:b54539d980a6319156ec7c"
+};
+
+// Initialize Firebase
+const loginApp = initializeApp(firebaseConfig);
+
+
 
 function createUser( userInfo) {
+  const auth = getAuth(loginApp);
   return new Promise( (resolve, reject) => {  
     createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
       .then( userCredential => resolve(userCredential))
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,22 +1,22 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
-
+//intialize App
 const firebaseConfig = {
-  "apiKey": "AIzaSyD6Q4GR2i-LZmeBhLgkp7tjt99yu54WIQQ",
-  "authDomain": "badbank-d105e.firebaseapp.com",
-  "projectId": "badbank-d105e",
-  "storageBucket": "badbank-d105e.appspot.com",
-  "messagingSenderId": "953333433177",
-  "appId": "1:953333433177:web:de4ae1b9202129cfc60248"  
-}
+  apiKey: "AIzaSyCIdgP5UepZYNJfegdUxhEaZl_lCNz3m8U",
+  authDomain: "badbank-87112.firebaseapp.com",
+  projectId: "badbank-87112",
+  storageBucket: "badbank-87112.appspot.com",
+  messagingSenderId: "641621008693",
+  appId: "1:641621008693:web:b54539d980a6319156ec7c"
+};
 
-const app = initializeApp(firebaseConfig)
+const loginApp = initializeApp(firebaseConfig);
 
 
 function doLogin (userInfo) {
   return new Promise((resolve, reject) => {
-    const auth  = getAuth(app)
+    const auth  = getAuth(loginApp)
     const promise = signInWithEmailAndPassword( auth,
       userInfo.email, userInfo.password);
 
